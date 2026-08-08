9f5150acc00f28ce18113a2f3a41f3908950effe
p <pmamaril@g.ucla.edu>
2026-08-08 14:38:11 -0700

    Add: maximum-product-subarray - submission-2

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        res = float("-inf")
+        # start at the second element.
+        for i in range(len(nums) - 2, -1, -1):
+            nums[i] = max(nums[i], nums[i] * nums[i + 1])
+            res = max(res, nums[i])
+        
+        return max(nums[0], res)
