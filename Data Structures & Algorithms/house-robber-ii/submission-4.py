3e9ee7f0aa17cdd976b141d679cdab0520acb476
p <pmamaril@g.ucla.edu>
2026-08-08 10:18:02 -0700

    Add: house-robber-ii - submission-4

---
+++ Data Structures & Algorithms/house-robber-ii/submission-4.py
@@ -0,0 +1,31 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def helper(arr):
+            arr.append(0)
+            for i in range(len(nums) - 4, -1, -1):
+                arr[i] += max(arr[i + 2], arr[i + 3])
+            
+
+            if len(arr) > 1 and arr[1] > arr[0]:
+                return arr[1]
+            else:
+                return arr[0]
+
+        return max(nums[0], helper(nums[1:]), helper(nums[:-1]))
+
+            
+
+            
+
+
+            
+
+        
+
+
+    
+        
+
+
+
+
