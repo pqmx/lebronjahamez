8877bd77e908f41b6ac4a966f3a28b3b10d24873
p <pmamaril@g.ucla.edu>
2026-08-16 17:32:52 -0700

    Add: valid-tree - submission-9

---
+++ Data Structures & Algorithms/valid-tree/submission-9.py
@@ -0,0 +1,32 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) > n - 1: # complete graphs has n -1 nodes
+            return False
+
+        rel = [[] for _ in range(n)]
+        #visit all nodes.
+    
+
+        for src, dest in edges:
+            rel[src].append(dest)
+            rel[dest].append(src)
+        
+        visit = set()
+        q = deque([(0, -1)])
+        visit.add(0)
+
+        while q:
+            node, p = q.popleft()
+            for ve in rel[node]:
+                if ve == p:
+                    continue
+                if ve in visit:
+                    return False
+                visit.add(ve)
+                q.append((ve, node))
+
+        return len(visit) == n
+
+
+
+
