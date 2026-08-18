4d8244fd2417982616ab03295ab3d58ba528cad2
p <pmamaril@g.ucla.edu>
2026-08-18 15:42:19 -0700

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        cars = [[position[i], speed[i]] for i in range(len(position))]
+        cars = cars.sort(key=lambda x: x[0], reverse=True)
+
+        stack = []
+        print(cars)
+        for p, s in enumerate(cars):
+            curTime = (target-p) / s
+            stack.append(curTime)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+        
+        return len(stack)
+                
+
+
+
+                
+
+
+        
+
+
+        
