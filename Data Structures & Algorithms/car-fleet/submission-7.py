5fce04ded37f3c48129aa3684a5a41cd519c8038
p <pmamaril@g.ucla.edu>
2026-08-18 15:48:50 -0700

    Add: car-fleet - submission-7

---
+++ Data Structures & Algorithms/car-fleet/submission-7.py
@@ -0,0 +1,24 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        cars = [[position[i], speed[i]] for i in range(len(position))]
+        cars.sort(key=lambda x: x[0], reverse=True)
+
+        stack = []
+        for p, s in cars:
+            curTime = (target-p) / s
+            stack.append(curTime)
+            if len(stack) >= 2 and curTime <= stack[-2]:
+                stack.pop()
+        
+        return len(stack)
+                
+
+
+
+                
+
+
+        
+
+
+        
