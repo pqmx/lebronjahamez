6026cb8c2a4f8a9f985e1673e34db59cb40918b4
p <pmamaril@g.ucla.edu>
2026-08-25 11:28:47 -0700

    Add: valid-parenthesis-string - submission-1

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        # lets do whatever is needed.
+        left = []
+
+        lives = []
+
+        for i in range(len(s)):
+            c = s[i]
+            if c == '(':
+                left.append(i)
+            elif c == ")":
+                if left:
+                    if lives and lives[-1] > left[-1]:
+                        lives.pop()
+                    else:
+                        left.pop()
+                else:
+                    if lives:
+                        lives.pop()
+                    else:
+                        return False
+            else: # special clause.
+                lives.append(i)
+        
+
+        return not left
+        
