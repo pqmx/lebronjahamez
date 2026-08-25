3979f6135dca91aac4dd7f2cd667e61e05bf3408
p <pmamaril@g.ucla.edu>
2026-08-25 13:51:22 -0700

    Add: non-cyclical-number - submission-0

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        
+        
+        visited = set()
+        while n != 1 and n not in visited:
+            total = 0
+            while n >= 10:
+                remainder = n % 10
+                total += (remainder * remainder)
+                n //= 10
+            total += n * n
+            n = total
+            visited.add(n)
+        
+
+        if n == 1:
+            return True
+        return False
+
+
+        
+        
