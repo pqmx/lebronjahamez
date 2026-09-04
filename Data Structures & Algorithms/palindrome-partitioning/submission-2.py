206e4995657257917f61047209668d1e9ac09fc0
p <pmamaril@g.ucla.edu>
2026-09-04 10:29:56 -0700

    Add: palindrome-partitioning - submission-2

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-2.py
@@ -0,0 +1,48 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        n = len(s)
+        res = []
+
+
+        def isPalindrome(s):
+            check = len(s) // 2 
+            last = len(s) - 1
+            for i in range(check):
+                if s[i] != s[last - i]:
+                    return False
+            return True
+
+
+        def dfs(i, arr):
+            if i >= n:
+                res.append(arr[:])
+                return
+            
+
+            #separate
+            arr.append(s[i])
+            dfs(i + 1, arr)
+
+            arr.pop()
+
+            #include 
+            if not arr:
+                return
+
+            temp = arr[-1][:]
+            temp += s[i]
+
+
+            print(temp)
+            if isPalindrome(temp):
+               
+                arr[-1] += s[i]
+                dfs(i + 1, arr)
+
+
+        dfs(0, [])
+
+        return res
+
+
+        
