2be7fd3a7d9275b14f5351975645ffdcfd5529b9
p <pmamaril@g.ucla.edu>
2026-09-04 18:05:56 -0700

    Add: target-sum - submission-8

---
+++ Data Structures & Algorithms/target-sum/submission-8.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        memo = [None] * (n + 1)
+        def dfs(i, cur):
+            if i >= n:
+                if cur == target:
+                    return 1
+                return 0
+            
+
+            if memo[i] is not None:
+                return memo[i]
+            curNum = nums[i]
+            memo[i] = dfs(i + 1, cur + curNum) + dfs(i + 1, cur - curNum)
+            return memo[i]
+        
+
+        return dfs(0, 0)
+        
+            
+
+
+
+
+        
