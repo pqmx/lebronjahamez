96bc9dcf61c6b5412f642d3210cc37e311175483
p <pmamaril@g.ucla.edu>
2026-09-06 09:09:16 -0700

    Add: cheapest-flight-path - submission-3

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-3.py
@@ -0,0 +1,36 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        adj = {}
+        res = float("inf")
+        for start, end, price in flights:
+            if start not in adj:
+                adj[start] = []
+            adj[start].append([price, end])
+
+
+
+        #design fo heap price, start, stops
+        h = [[0, src, -1]]
+        while h:
+            curPrice, start, curStops = heapq.heappop(h)
+
+            if curStops > k:
+                continue
+            if dst == start: # we have landed at our destination.
+                res = min(res, curPrice)
+                continue
+
+            if start not in adj:
+                continue
+            for price, end in adj[start]:
+                heapq.heappush(h, [price + curPrice, end, curStops + 1])
+            visited.add(start)
+
+
+
+        if res == float("inf"):
+            return -1
+        return res
+
+
+
