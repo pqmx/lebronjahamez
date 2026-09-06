fb2628feb685797525cdc4506cf371ecf0b8fd96
p <pmamaril@g.ucla.edu>
2026-09-06 13:34:11 -0700

    Add: insert-new-interval - submission-19

---
+++ Data Structures & Algorithms/insert-new-interval/submission-19.py
@@ -0,0 +1,50 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
+        newStart, newEnd = newInterval
+        hasInserted = False
+        for curStart, curEnd in intervals:
+            arr = [curStart, curEnd]
+            if not res:
+                res.append(arr)
+                continue
+
+
+            prevStart, prevEnd = res[-1]
+
+            # merge newInterval -> 
+            if not hasInserted and prevEnd >= newStart:
+                newArr = [min(prevStart, newStart), max(prevEnd, newEnd)]
+                res.pop()
+                res.append(newArr)
+                hasInserted = True
+
+            # only append if we can
+            if not hasInserted and prevEnd <= newStart <= curStart:
+                    hasInserted = True
+                    res.append(newInterval)
+
+            # lastly append the cur element but we must still check.
+            prevStart, prevEnd = res[-1]
+            if prevEnd >= curStart:
+                arr = [min(prevStart, curStart), max(prevEnd, curEnd)]
+                res.pop()
+                res.append(arr)
+            else:
+                res.append(arr)
+            
+
+
+        if not hasInserted:
+            res.append(newInterval)
+        return res
+
+            
+            
+        
+
+
+
+
+    
+        
