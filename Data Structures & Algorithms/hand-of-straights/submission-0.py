11c6e63455d969eccbb05b7b57649ca7ec189f38
p <pmamaril@g.ucla.edu>
2026-09-06 11:56:25 -0700

    Add: hand-of-straights - submission-0

---
+++ Data Structures & Algorithms/hand-of-straights/submission-0.py
@@ -0,0 +1,32 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize != 0:
+            return False
+        
+
+
+        hsh = {}
+        for h in hand:
+            hsh[h] = hsh.get(h, 0) + 1
+        
+        
+
+        for h in sorted(hsh):
+            if h - 1 not in hsh: # we found our base.
+                for i in range(1, groupSize):
+                    testNum = h + i
+                    if testNum not in hsh:
+                        return False
+                    else:
+                        hsh[testNum] -= 1
+                        if hsh[testNum] <= 0:
+                            del hsh[testNum]
+        
+        return True
+
+                    
+
+        
+            
+           
+
