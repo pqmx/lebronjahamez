c3b94ff46cd9882938e8867074598f14014df2e7
p <pmamaril@g.ucla.edu>
2026-09-08 18:59:32 -0700

    Add: buy-and-sell-crypto-with-cooldown - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-0.py
@@ -0,0 +1,40 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        res = 0
+        def dfs(i, cur, canBuy):
+            nonlocal res
+            if i >= len(prices):
+                res = max(cur, res)
+                return res
+            total = 0
+            if canBuy:
+                buy = dfs(i + 1, cur - prices[i], False) # we buy, subtract from our profit. lose buying power.
+                skip = dfs(i + 1, cur, True) # we skip nothing happens
+                total = max(buy, skip)
+            else:
+                # we need to sell or skip selling
+                sell = dfs(i + 2, cur + prices[i], True) # buying power granted + skips next day for us.
+                skip = dfs(i + 1, cur, False) # nothing changes.
+                total = max(sell, skip)
+            
+
+
+            res = max(total, res)
+            return res
+
+        dfs(0, 0, True)
+        return res
+        
+
+
+
+
+
+
+
+
+
+        dfs(0, 0, True)
+
+        
