b95126a6008f004cc07255966d95f1d3c86d2d6f
p <pmamaril@g.ucla.edu>
2026-09-21 14:12:41 -0700

    Add: dota2-senate - submission-3

---
+++ Data Structures & Algorithms/dota2-senate/submission-3.py
@@ -0,0 +1,26 @@
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        d, r = deque(), deque()
+
+
+        # the name fo the game: ban every single person last team remaining wins.
+        for i in range(len(senate)):
+            if senate[i] == 'R':
+                r.append(i)
+            else:
+                d.append(i)
+        
+        while d and r:
+            if d[0] < r[0]:
+                r.popleft()
+
+            else:
+                d.popleft()
+        
+
+
+        print(d)
+        print(r)
+        return "Dire" if len(d) > len(r) else "Radiant"
+
+        
