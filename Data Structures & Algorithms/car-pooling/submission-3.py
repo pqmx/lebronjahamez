8cfc5532fdf145013b433d11c5a523328e20b573
p <pmamaril@g.ucla.edu>
2026-09-22 16:38:11 -0700

    Add: car-pooling - submission-3

---
+++ Data Structures & Algorithms/car-pooling/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        trips.sort(key = lambda x : x[1])
+        minHeap = []
+        curCap = 0
+        for curPass, start, end in trips:
+            # check whether we are at a stop to drop off.
+            while minHeap and minHeap[0][0] <= start:
+                stop, passengerDrop = minHeap.pop()
+                curCap -= passengerDrop
+            # we add the passengers.
+            if curCap > capacity:
+                return False
+            curCap += curPass
+            if curCap > capacity:
+                return False
+            heapq.heappush(minHeap, (end, curPass))
+        
+
+        return True
+            
+
+        
+
+        
