53fde63a9fa22f155ba7fe00122b02c37f8d9c9d
p <pmamaril@g.ucla.edu>
2026-09-24 15:36:14 -0700

    Add: 4sum - submission-1

---
+++ Data Structures & Algorithms/4sum/submission-1.py
@@ -0,0 +1,50 @@
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        n = len(nums)
+
+        res = []
+        for i in range(n):
+            if i >= n - 3: # if i is the second to last (not possible anymore to make a sum w/ 4 numbers.)
+                break
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            firstNum = nums[i]
+            for j in range(i + 1, n):
+                if j >= n - 2: # if j is the second to last (not possible anymore to make a sum w/ 4 numbers.)
+                    break
+                secondNum = nums[j]
+                remain = target - (firstNum + secondNum)
+                
+                
+                # two pointers
+                l, r = j + 1, n - 1
+
+                # l cannot overlap r since it is 2 distinct nums (must)
+                while l < r:
+                    amt = nums[l] + nums[r]
+                    if amt > remain:
+                        r -= 1
+                    elif amt < remain:
+                        l += 1
+                    else:
+                        res.append([firstNum, secondNum, nums[l], nums[r]])
+                        break
+        return res
+
+
+
+                
+
+                
+
+
+                
+
+        
+
+
+
+        
+
+        
